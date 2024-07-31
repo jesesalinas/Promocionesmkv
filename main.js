776f5ffe461f8022b9432a8467dab536e0c717e4
jesesalinas <jesesalinas5@gmail.com>
2024-07-31 11:18:09 -0600

    CAMBIO

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      autoplay: {
-        delay: 3000, // Cambia el valor para ajustar el tiempo entre transiciones
-        disableOnInteraction: false, // Para que siga el autoplay aunque el usuario interactúe
-      },
-      breakpoints: {
-        576: {
-          slidesPerView: 3,
-          spaceBetween: 20,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        slidesPerGroup: 1, // Cambia esta opción para ajustar el número de tarjetas que se mueven por vez
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        992: {
-          slidesPerView: 4,
-          spaceBetween: 30,
+        autoplay: {
+            delay: 5000, // Cambia el valor para ajustar el tiempo entre transiciones
+            disableOnInteraction: false, // Para que siga el autoplay aunque el usuario interactúe
         },
-      },
+        breakpoints: {
+            576: {
+                slidesPerView: 3,
+                slidesPerGroup: 3, // Ajusta para que el grupo de diapositivas sea de 3
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 4,
+                slidesPerGroup: 4, // Ajusta para que el grupo de diapositivas sea de 4
+                spaceBetween: 30,
+            },
+        },
     });
-  });
+});
   
   
   
